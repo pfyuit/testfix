27b445742b341480a29a4a13b384d33f5d0a6284
pfyuit <pfyuit@gmail.com>
2014-04-16 14:09:02 +0800

    Revert "l"
    This reverts commit 6442048ad80c6afaf1461eecc932ecc338d7e49c.

--- TestFixWeb/sample/a.sql
+++ TestFixWeb/sample/a.sql
@@ -1,8 +1,4 @@
 create table User(
   name varchar(50) primary key,
-  notes varchar(100) null,
-  age int not null,
-  sex char(1) not null,
-  address varchar(500),
-  phone varchar(50)
+  notes varchar(100) null
 )
