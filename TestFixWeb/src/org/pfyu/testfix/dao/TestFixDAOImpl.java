f44b9d5b2a20b6fd4751933ebcf20882d7ab59ea
pfyuit <pfyuit@gmail.com>
2014-04-16 11:47:59 +0800

    added new status

--- TestFixWeb/src/org/pfyu/testfix/dao/TestFixDAOImpl.java
+++ TestFixWeb/src/org/pfyu/testfix/dao/TestFixDAOImpl.java
@@ -14,6 +14,8 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class TestFixDAOImpl implements TestFixDAO{
 	
+	public static final String STATUS = "hello";
+	
 	protected static final class NamedQueries {
 		protected static final String getTestfixes = "SELECT c FROM Testfix c";
 	}
